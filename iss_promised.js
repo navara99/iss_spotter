20c60b8f9a9b3fe191c9ff20c90338a20279b385
Tharsikan <thar.navaratnam@gmail.com>
2021-11-04 20:57:34 +0000

    implemented promise based solution

---
+++ iss_promised.js
@@ -0,0 +1,29 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = () => {
+  return request("https://api.ipify.org/?format=json");
+}
+
+const fetchCoordsByIP = (data) => {
+  const ip = JSON.parse(data).ip;
+  return request(`https://freegeoip.app/json/${ip}`)
+}
+
+const fetchISSFlyOverTimes = (data) => {
+  const { latitude, longitude } = JSON.parse(data);
+  return request(`https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`)
+};
+
+const nextISSTimesForMyLocation = () => {
+
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+
+};
+
+module.exports = nextISSTimesForMyLocation;

86388b078bcfdb97fa4abf7824caee2760da0756
Tharsikan <thar.navaratnam@gmail.com>
2021-11-04 18:25:18 +0000

    eslint fix

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const {fetchMyIP} = require("./iss");
 
 fetchMyIP((err,ip)=> {
   if (err) {
-    console.log("It didn't work" , error);
-    return 
+    console.log("It didn't work" , err);
+    return;
   }
 
   console.log('It worked! Returned IP:' , ip);
